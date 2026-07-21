1711974253387beabfa692e3c1eb64def4d5918b
aluguvelliapoorvareddy-creator <aluguvelliapoorvareddy@gmail.com>
2026-07-21 13:49:35 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
 print("hello,codomax AI Internship!")
 print("environment setup is complete and ready.")
-print("Develgit add day1.pyopment environment is ready .submit todays task via google form")
+
 print("Development environment is ready .submit todays task via google form")
